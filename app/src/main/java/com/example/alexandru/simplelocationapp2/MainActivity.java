95b95fae1c2e54f3ad10026c3752065af5aac07a
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-06-10 12:19:09 +0300

    update 4

--- app/src/main/java/com/example/alexandru/simplelocationapp2/MainActivity.java
+++ app/src/main/java/com/example/alexandru/simplelocationapp2/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.alexandru.simplelocationapp2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+
+    protected GoogleApiClient googleApiClient;
+    protected Location mLastLocation;
+    protected TextView latValue;
+    protected TextView longValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        latValue = (TextView) findViewById(R.id.lat_value);
+        longValue = (TextView) findViewById(R.id.long_value);
+        buildGoogleAppClient();
+    }
+
+    private void buildGoogleAppClient() {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
