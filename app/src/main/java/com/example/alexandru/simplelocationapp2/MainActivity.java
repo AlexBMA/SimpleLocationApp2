4dd51df91324d20bda1af5b0f71c35e5d7c1c061
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-06-10 14:30:06 +0300

    update 9

--- app/src/main/java/com/example/alexandru/simplelocationapp2/MainActivity.java
+++ app/src/main/java/com/example/alexandru/simplelocationapp2/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.alexandru.simplelocationapp2;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
 
+    protected final String LOG_TAG = String.valueOf(R.string.app_name);
     protected GoogleApiClient mGoogleApiClient;
     protected Location mLastLocation;
+    protected LocationRequest mLocationRequest;
+
     protected TextView latValue;
     protected TextView longValue;
 
@@ -58,6 +65,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
             latValue.setText(String.valueOf(mLastLocation.getLatitude()));
@@ -68,12 +84,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.e(LOG_TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.e(LOG_TAG, "Connection " + connectionResult.getErrorMessage() + " " + connectionResult.getErrorCode());
     }
 
     @Override
